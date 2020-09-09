3015131ed79a6d59450105af93be4a762572ade7
isaac93romano@gmail.com <isaac93romano@gmail.com>
2020-09-09 12:29:56 -0400

    modified App.js

--- frontend/dolphinvintage/src/App.js
+++ frontend/dolphinvintage/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Route, Redirect } from 'react-router-dom';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			itemData: [],
+		};
+	}
 
+	componentDidMount() {
+		axios('https://new-dolphin-backend.herokuapp.com/items/')
+			.then((json) => {
+				console.log(json);
+				this.setState({
+					itemData: json,
+				});
+			})
+			.catch(console.error);
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<main>
+					<p className='title'>Dolphin Vintage</p>
+					<Route
+						path='/'
+						exact
+						render={() => {
+							return (
+								<div>{/* <ItemsList itemData={this.state.itemData} /> */}</div>
+							);
+						}}
+					/>
+				</main>
+			</div>
+		);
+	}
+}
 export default App;
